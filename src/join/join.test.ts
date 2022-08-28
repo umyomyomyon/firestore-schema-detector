9aae23f1f31e190b606633cb86cb8d01a4be107b
umyomyomyon <minecraft.sentry.yym@gmail.com>
2022-08-28 18:01:57 +0900

    テスト追加

--- src/join/join.test.ts
+++ src/join/join.test.ts
@@ -152,4 +152,222 @@ describe("joinConvertedDocs tests", () => {
     const intergrater = new DocsIntegrater(convertedDocs);
     expect(intergrater.joinDocs()).toStrictEqual([joinResult, joinMeta]);
   });
+
+  it("one object", () => {
+    const convertedDocs: ConvertedDocument[] = [
+      {
+        map: {
+          string: "string",
+          boolean: "boolean",
+        },
+      },
+    ];
+    const joinResult: JoinResult = {
+      map: [
+        {
+          string: "string",
+          boolean: "boolean",
+        },
+      ],
+    };
+    const joinMeta: JoinMeta = {
+      optionals: [],
+    };
+    const intergrater = new DocsIntegrater(convertedDocs);
+    expect(intergrater.joinDocs()).toStrictEqual([joinResult, joinMeta]);
+  });
+
+  it("one deep object", () => {
+    const convertedDocs: ConvertedDocument[] = [
+      {
+        map: {
+          map: {
+            map: {
+              map: {
+                map: {
+                  map: {
+                    string: "string",
+                    boolean: "boolean",
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    ];
+    const joinResult: JoinResult = {
+      map: [
+        {
+          map: {
+            map: {
+              map: {
+                map: {
+                  map: {
+                    string: "string",
+                    boolean: "boolean",
+                  },
+                },
+              },
+            },
+          },
+        },
+      ],
+    };
+    const joinMeta: JoinMeta = {
+      optionals: [],
+    };
+    const intergrater = new DocsIntegrater(convertedDocs);
+    expect(intergrater.joinDocs()).toStrictEqual([joinResult, joinMeta]);
+  });
+
+  it("has objects", () => {
+    const convertedDocs: ConvertedDocument[] = [
+      {
+        map1: {
+          string: "string",
+          boolean: "boolean",
+        },
+        map2: {
+          null: "null",
+        },
+      },
+    ];
+    const joinResult: JoinResult = {
+      map1: [
+        {
+          string: "string",
+          boolean: "boolean",
+        },
+      ],
+      map2: [
+        {
+          null: "null",
+        },
+      ],
+    };
+    const joinMeta: JoinMeta = {
+      optionals: [],
+    };
+    const intergrater = new DocsIntegrater(convertedDocs);
+    expect(intergrater.joinDocs()).toStrictEqual([joinResult, joinMeta]);
+  });
+
+  it("same objects", () => {
+    const convertedDocs: ConvertedDocument[] = [
+      {
+        map: {
+          string: "string",
+          boolean: "boolean",
+        },
+      },
+      {
+        map: {
+          string: "string",
+          boolean: "boolean",
+        },
+      },
+    ];
+    const joinResult: JoinResult = {
+      map: [
+        {
+          string: "string",
+          boolean: "boolean",
+        },
+        {
+          string: "string",
+          boolean: "boolean",
+        },
+      ],
+    };
+    const joinMeta: JoinMeta = {
+      optionals: [],
+    };
+    const intergrater = new DocsIntegrater(convertedDocs);
+    expect(intergrater.joinDocs()).toStrictEqual([joinResult, joinMeta]);
+  });
+
+  it("all optional", () => {
+    const convertedDocs: ConvertedDocument[] = [
+      {
+        map: {
+          string: "string",
+          boolean: "boolean",
+        },
+      },
+      {
+        pam: {
+          string: "string",
+          boolean: "boolean",
+        },
+      },
+    ];
+    const joinResult: JoinResult = {
+      map: [
+        {
+          string: "string",
+          boolean: "boolean",
+        },
+      ],
+      pam: [
+        {
+          string: "string",
+          boolean: "boolean",
+        },
+      ],
+    };
+    const joinMeta: JoinMeta = {
+      optionals: ["map", "pam"],
+    };
+    const intergrater = new DocsIntegrater(convertedDocs);
+    expect(intergrater.joinDocs()).toStrictEqual([joinResult, joinMeta]);
+  });
+
+  it("partial optional", () => {
+    const convertedDocs: ConvertedDocument[] = [
+      {
+        string: "string",
+        map: {
+          string: "string",
+          boolean: "boolean",
+        },
+        boolean: "boolean",
+      },
+      {
+        string: "boolean",
+        map: {
+          array: "array",
+          timastamp: "timestamp",
+        },
+        boolean: "boolean",
+        opt: {
+          null: "null",
+        },
+      },
+    ];
+    const joinResult: JoinResult = {
+      string: ["string", "boolean"],
+      map: [
+        {
+          string: "string",
+          boolean: "boolean",
+        },
+        {
+          array: "array",
+          timastamp: "timestamp",
+        },
+      ],
+      boolean: ["boolean"],
+      opt: [
+        {
+          null: "null",
+        },
+      ],
+    };
+    const joinMeta: JoinMeta = {
+      optionals: ["opt"],
+    };
+    const intergrater = new DocsIntegrater(convertedDocs);
+    expect(intergrater.joinDocs()).toStrictEqual([joinResult, joinMeta]);
+  });
 });
