fdfcf44f70055c14f64af0e5f2eb6c5d5ef12128
umyomyomyon <minecraft.sentry.yym@gmail.com>
2022-08-31 10:33:19 +0900

    メイン処理を修正

--- src/firestore.ts
+++ src/firestore.ts
@@ -1,24 +1,11 @@
 import { firestore } from "firebase-admin";
+import { DocumentData } from "firebase-admin/firestore";
 
-export const getDocumentData = async (
-  collectionName: string,
-  documentId: string
-): Promise<firestore.DocumentData | undefined> => {
+export const getDocuments = async (
+  collectionName: string
+): Promise<DocumentData[]> => {
   const fs = firestore();
-  const docRef = fs.collection(collectionName).doc(documentId);
-  const docSnapShot = await docRef.get();
-  const docData = docSnapShot.data();
-  return docData;
-};
-
-export const checkTimeStamp = (arg: firestore.DocumentData | undefined) => {
-  if (!arg) return;
-  if (arg.timestampField) {
-    const fsTimeStamp = arg.timestampField;
-    const dateFromFirestoreTimeStamp = arg.timestampField.toDate();
-    console.log(dateFromFirestoreTimeStamp);
-    console.log(fsTimeStamp instanceof firestore.FieldValue);
-    console.log(dateFromFirestoreTimeStamp instanceof Date);
-    console.log(fsTimeStamp);
-  }
+  const ref = fs.collection(collectionName);
+  const snapshot = await ref.get();
+  return snapshot.docs.map((doc) => doc.data());
 };
